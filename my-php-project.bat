e3b01b6cbc3b6b37f223a651386f95216453ffd9
Giantinos <l1on.free@yandex.ru>
2026-04-14 14:35:46 +0300

    fixed config to deploy 0.1

--- my-php-project.bat
+++ my-php-project.bat
@@ -13,7 +13,7 @@ echo COPY default.conf /etc/nginx/conf.d/default.conf
 :: конфиг Nginx для php-fpm
 (echo server {
 echo     listen 80;
-echo     root /var/www/html/public;
+echo     root /var/www/html;
 echo     index index.php index.html;
 echo. 
 echo     location / {
@@ -23,7 +23,7 @@ echo.
 echo     location ~ \.php$ {
 echo         fastcgi_pass php:9000;  # Указываем сервис PHP
 echo         fastcgi_index index.php;
-echo         fastcgi_index index.php;
+echo         include fastcgi_params;
 echo         fastcgi_param SCRIPT_FILENAME
 echo         $document_root$fastcgi_script_name;
 echo     }
@@ -37,7 +37,7 @@ echo RUN docker-php-ext-install pdo pdo_mysql mysqli
 echo # "Копируем кастомный php.ini (если есть)"
 echo COPY php.ini /usr/local/etc/php/
 echo # "Устанавливаем Composer (опционально)"
-echo COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+echo #COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 ) > %PROJECT_NAME%\docker\php\Dockerfile
 
 type nul > %PROJECT_NAME%\docker\php\php.ini
@@ -49,34 +49,34 @@ mkdir %PROJECT_NAME%\database
 
 (
 echo services:
-echo  nginx:
-echo    build: ./docker/nginx
-echo    ports:
-echo      - "80:80"
-echo    volumes:
-echo      - #ABSOLUTE_PATH/%PROJECT_NAME%/app:/var/www/html
-echo      - ./logs/nginx:/var/log/nginx
-echo    depends_on:
-echo      - php
-echo.
-echo.
-echo  php:
-echo    build: ./docker/php
-echo    volumes:
-echo      - #ABSOLUTE_PATH/%PROJECT_NAME%/app:/var/www/html
-echo      - ./docker/php/php/ini:/user/local/etc/php/conf.d/custom.ini
-echo.
-echo.
-echo  mysql:
-echo    image: mysql:8.0
-echo    environment:
-echo      MYSQL_DATABASE: myapp
-echo      MYSQL_USER: appuser
-echo      MYSQL_PASSWORD: appadmin
-echo    volumes:
-echo      - #ABSOLUTE_PATH/%PROJECT_NAME%/database:/var/lib/mysql
-echo      - ./dockermysql/init:/docker-entrypoint-initdb.d
+echo   nginx:
+echo     build: ./docker/nginx
+echo     ports:
+echo       - "80:80"
+echo     volumes:
+echo       - ./app/public:/var/www/html
+echo       - ./logs/nginx:/var/log/nginx
+echo     depends_on:
+echo       - php
+echo. 
+echo. 
+echo   php:
+echo     build: ./docker/php
+echo     volumes:
+echo       - ./app/public:/var/www/html
+echo       - ./docker/php/php/ini:/usr/local/etc/php/conf.d/custom.ini
+echo. 
+echo. 
+echo   mysql:
+echo     image: mysql:8.0
+echo     environment:
+echo       MYSQL_DATABASE: myapp
+echo       MYSQL_USER: appuser
+echo       MYSQL_PASSWORD: appadmin
+echo     volumes:
+echo       - ./database:/var/lib/mysql
+echo       - ./dockermysql/init:/docker-entrypoint-initdb.d
 ) > %PROJECT_NAME%\docker-compose.yml
 
-echo "<?php echo %PROJECT_NAME%" > %PROJECT_NAME%\app\public\index.php
+echo "<?php echo 'Hello from %PROJECT_NAME%'?>" > %PROJECT_NAME%\app\public\index.php
 pause
